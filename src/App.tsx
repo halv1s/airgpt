3a5b41d22d7f37b0d9b955c9b8a763f670eb91a3
halv1s <dev.ngtronghieu@gmail.com>
2024-10-22 01:30:35 +0700

    chore: handle input on change

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Textarea } from "./components/ui/textarea";
 import { Button } from "./components/ui/button";
 import Message from "./components/message";
+import { useState } from "react";
 
 const markdownMessage = `
   # Hello, World!
@@ -12,6 +13,17 @@ const markdownMessage = `
 `;
 
 function App() {
+    const [isSending, setIsSending] = useState(false);
+    const [input, setInput] = useState("");
+
+    const handleSendInput = async () => {
+        setIsSending(true);
+        console.log("--> send:", input);
+        await new Promise((resolve) => setTimeout(resolve, 1000));
+        setInput("");
+        setIsSending(false);
+    };
+
     return (
         <div className="flex-grow flex flex-col">
             <div className="flex-grow">
@@ -19,8 +31,14 @@ function App() {
             </div>
 
             <div className="p-4 flex gap-4">
-                <Textarea />
-                <Button>Send</Button>
+                <Textarea
+                    value={input}
+                    onChange={(e) => setInput(e.target.value)}
+                    disabled={isSending}
+                />
+                <Button onClick={handleSendInput} disabled={isSending}>
+                    Send
+                </Button>
             </div>
         </div>
     );
